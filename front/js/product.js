39d58a4dabae182cf9c9b2949749ecd3b6c505df
Drelkan <sebastien.poret@hotmail.fr>
2023-02-24 16:08:06 +0100

    modification

--- front/js/product.js
+++ front/js/product.js
@@ -25,24 +25,12 @@ fetch("http://localhost:3000/api/products/" + productId)
         title.innerHTML = product.name
         price.innerHTML = product.price
         description.innerHTML = product.description
-        // colors.innerHTML = product.colors
 
-        // Récupérer le select grâce à l'id qui colors
         const select = document.getElementById('colors')
-
-        // Pour chaque élément du tableau colors issu de product
         product.colors.forEach(color => {
-
-            // Créer une option dans le DOM qui va pouvoir être ajouter dans le select
             const opt = document.createElement("option");
-
-            // Assigner le nom de la color à la value
             opt.value = color;
-
-            // Assigner le nom de la color au text
             opt.text = color;
-
-            // Ajouter l'option dans le select
             select.add(opt)
         })
     })
