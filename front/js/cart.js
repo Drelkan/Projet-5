e4bdf013796d54b0ab7c7ed446b3b4bf4a80d9f6
Drelkan <sebastien.poret@hotmail.fr>
2023-03-16 15:48:14 +0100

    test regex

--- front/js/cart.js
+++ front/js/cart.js
@@ -2,7 +2,9 @@ let cart = JSON.parse(localStorage.getItem("cart"))
 
 const cartItems = document.getElementById("cart__items")
 let totalQuantities = 0
+const totalQuantitiesELement = document.getElementById("totalQuantity")
 let totalPrices = 0
+const totalPriceElement = document.getElementById("totalPrice")
 
 
 
@@ -38,68 +40,50 @@ for (const cartItem of cart) {
 
             // ajout des quantitées et boutton supprimer
             const deleteButtons = document.getElementsByClassName("deleteItem")
-            for(var i = 0; i < deleteButtons.length; i++) {
-              (function(index) {
-                deleteButtons[index].addEventListener("click", function() {
-                   cart.splice(index, 1)
-                   localStorage.setItem("cart", JSON.stringify(cart))
-                   const e = document.querySelector(`[data-id="${product._id}"]`)
-                   e.remove();
-                  })
-              })(i);
-            }
+            Object.values(deleteButtons).forEach( (deleteButton, index) => {
+                let article = deleteButton.closest("article")
+                console.log("deleteButtons", deleteButton)
+                const productId = article.getAttribute("data-id")
+                const productColor = article.getAttribute("data-color")
+                deleteButton.addEventListener("click", function(event){
+                  // deleteButtons[index].onClick = function(){alert("Finaly!");};
+                    // splice
+                  console.log("deleteButtons", article)
+                  let indexProductForRemove = cart.findIndex((object => object.id === productId && object.color === productColor))
+                  cart.splice(indexProductForRemove, 1)
+                  localStorage.setItem("cart", JSON.stringify(cart))
+                  alert("Le produit a bien été supprimer")
+                  location.reload()                    
+                })
+            })
 
+            const quantityImputs = document.getElementsByClassName("itemQuantity")
+            Object.values(quantityImputs).forEach( (quantityImput, index) => {
+              let article = quantityImput.closest("article")
+              const productId = article.getAttribute("data-id")
+              const productColor = article.getAttribute("data-color")
+              quantityImput.addEventListener("change", function(event){
+                let currentQuantity = quantityImput.value
+                let indexProductForChangeQuantity = cart.findIndex((object => object.id === productId && object.color === productColor))
+                if(currentQuantity > 0 && currentQuantity <= 100){
+                  cart[indexProductForChangeQuantity].quantity = parseInt(currentQuantity)
+                  localStorage.setItem("cart", JSON.stringify(cart))
+                  alert("La quantité du produit a bien été modifier")
+                  location.reload()
+                }else{
+                  alert("Vueillez mettre une quantiter comprise entre 1 et 100")
+                }
 
-            // Object.values(deleteButtons).forEach( (deleteButton, index) => {
-                // let article = deleteButton.closest("article")
-                // console.log("deleteButtons", deleteButton)
-                // const productId = article.getAttribute("data-id")
-                // const productColor = article.getAttribute("data-color")
-            //     deleteButton.addEventListener("clic", function(event){
-            //       deleteButtons[index].onClick = function(){alert("Finaly!");};
-            //         // splice
-            //       console.log("deleteButtons", article)
+                
+              })
 
-            //       // const deleteButton = article.splice(quantity)
+          })
 
+          totalQuantities += parseInt(cartItem.quantity)
+          totalQuantitiesELement.innerHTML = totalQuantities
 
-                    
-            //     })
-
-
-            
-
-
-
-
-            // })
-
-           
-
-            // const totalPrice = async () => {
-            //   let lastProductId
-            //   let fetchProductJson
-            //   let productPrice = 0
-            //   for (let product of cart) {
-            //     if (product.id !== lastProductId){
-            //       productId = product.id
-            //       fetchProductJson = await fetchProduct(product.id)
-            //     }
-            //     productPrice += product.quantity * fetchProductJson.price
-            //   }
-            //   document.getElementById('totalPrice').textContent = productPrice
-            // }
-
-
-
-            // function totalQuantity () {
-            //   let number = 0;
-            //   for(let total of kanap) {
-            //     number += total.quantity;
-            //   }
-            //   return number
-            // }
-
+          totalPrices += parseInt(product.price)*parseInt(cartItem.quantity)
+          totalPriceElement.innerHTML = totalPrices
 
 
 
@@ -110,6 +94,38 @@ for (const cartItem of cart) {
 }
 
 // gestion formulaire de commande
+const form = document.getElementsByClassName("cart__order__form")[0]
+form.addEventListener("submit", function(event){
+  // console.log(form.elements)
+  event.preventDefault()
+  const inputs = form.elements
+  let validForm = 0
+  for(let input of inputs){
+    console.log(input)
+    let regexForField 
+    if(input.type === "submit"){
+      break
+    }
+    if(input.type === "text"){
+      regexForField = ""
+    }
+    if(input.type === "email"){
+      regexForField = ((/^((?!\.)[\w-_.]*[^.])(@\w+)(\.\w+(\.\w+)?[^.\W])$/).test(email.value) === false || email.value === "")
+      click.preventDefault()
+        msgError('emailErrorMsg');
+        return
+    }
+
+    if(input.value === ""){
+      input.style.border = "red 1px solid"
+      input.nextElementSibling.innerHTML = "Ce champ ne doit pas etre vide"
+    }
+
+
+  }
+
+
+})
 
 
 
